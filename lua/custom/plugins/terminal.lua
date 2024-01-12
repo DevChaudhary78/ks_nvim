f52148f4586b4ec11671e9b24bd9464cca909ace
Dev Chaudhary <devchaudhary.dc84.dc@gmail.com>
2024-01-12 16:34:13 -0500

    added the terminal toggle functionality

---
+++ lua/custom/plugins/terminal.lua
@@ -0,0 +1,11 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = "*",
+  opts = {
+    direction = 'float',
+    shell = "powershell",
+    terminal_mappings = true,
+    start_in_insert = true,
+    open_mapping = [[<c-\>]]
+  },
+}
