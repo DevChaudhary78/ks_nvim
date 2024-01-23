a6d8b235d7c0d7c554c216ac1f6ecb94be7f44b3
Dev Chaudhary <devchaudhary.dc84.dc@gmail.com>
2024-01-23 17:05:41 -0500

    fixed the bufferline config to show tabs

--- lua/custom/plugins/quality.lua
+++ lua/custom/plugins/quality.lua
@@ -56,14 +56,17 @@ return {
   {
     "akinsho/bufferline.nvim",
     event = "VeryLazy",
-    keys = {
-      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>",            desc = "Toggle pin" },
-      { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
-      { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>",          desc = "Delete other buffers" },
-      { "<leader>br", "<Cmd>BufferLineCloseRight<CR>",           desc = "Delete buffers to the right" },
-      { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>",            desc = "Delete buffers to the left" },
-      { "<S-h>",      "<cmd>BufferLineCyclePrev<cr>",            desc = "Prev buffer" },
-      { "<S-l>",      "<cmd>BufferLineCycleNext<cr>",            desc = "Next buffer" },
-    },
+    config = function()
+      require("bufferline").setup {
+        options = {
+          mode = "buffers",
+          buffer_close_icon = '󰅖',
+          modified_icon = '●',
+          close_icon = '',
+          left_trunc_marker = '',
+          right_trunc_marker = '',
+        }
+      }
+    end
   }
 }
