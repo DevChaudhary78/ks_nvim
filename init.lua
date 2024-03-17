baf17e0db69058653fa6adbc69af2e35cb187cc5
DevChaudhary78 <devchaudhary.dc84.dc@gmail.com>
2024-03-17 15:16:00 -0400

    Removed flash.nvim and remapped <Tab> selection to <C-y> selection

--- init.lua
+++ init.lua
@@ -532,7 +532,7 @@ cmp.setup {
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete {},
-    ['<Tab>'] = cmp.mapping.confirm {
+    ['<C-y>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
