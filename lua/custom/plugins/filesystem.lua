1e2b8b16b30c1ce22147ace22ffbffe52c6a388d
Dev Chaudhary <devchaudhary.dc84.dc@gmail.com>
2024-03-30 14:50:57 -0400

    Removed auto-session as it is replaced by TMUX for my config

--- lua/custom/plugins/filesystem.lua
+++ lua/custom/plugins/filesystem.lua
@@ -13,29 +13,29 @@ return {
     end
   },
   -- Session management using auto-session
-  {
-    'rmagatti/auto-session',
-    config = function()
-      require('auto-session').setup {
-        auto_session_enable_last_session = true,
-        log_level = 'info',
-        auto_session_root_dir = vim.fn.stdpath('data') .. "/sessions/",
-        auto_session_enabled = true,
-        auto_session_last_session_dir = vim.fn.stdpath('data') .. '/sessions/',
-        session_lens = {
-          buftypes_to_ignore = {},
-          load_on_setup = true,
-          theme_conf = { border = true },
-          previewer = false,
-        },
-      }
-
-      vim.keymap.set("n", "<C-s>", require("auto-session.session-lens").search_session, {
-        noremap = true,
-        desc = "Open the list of active sessions"
-      })
-    end
-  },
+  -- {
+  --   'rmagatti/auto-session',
+  --   config = function()
+  --     require('auto-session').setup {
+  --       auto_session_enable_last_session = true,
+  --       log_level = 'info',
+  --       auto_session_root_dir = vim.fn.stdpath('data') .. "/sessions/",
+  --       auto_session_enabled = true,
+  --       auto_session_last_session_dir = vim.fn.stdpath('data') .. '/sessions/',
+  --       session_lens = {
+  --         buftypes_to_ignore = {},
+  --         load_on_setup = true,
+  --         theme_conf = { border = true },
+  --         previewer = false,
+  --       },
+  --     }
+  --
+  --     vim.keymap.set("n", "<C-s>", require("auto-session.session-lens").search_session, {
+  --       noremap = true,
+  --       desc = "Open the list of active sessions"
+  --     })
+  --   end
+  -- },
   -- Harpoon to quickly toggle files
   {
     'ThePrimeagen/harpoon',
@@ -51,7 +51,7 @@ return {
       })
 
       for i = 1, 5 do
-        vim.api.nvim_set_keymap('n', '' .. i, [[:lua require("harpoon.ui").nav_file(]] .. i .. [[)<CR>]], {
+        vim.api.nvim_set_keymap('n', '<leader>' .. i, [[:lua require("harpoon.ui").nav_file(]] .. i .. [[)<CR>]], {
           noremap = true,
           desc = "Toggle through files using workspace keymaps"
         })
